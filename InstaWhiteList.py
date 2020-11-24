2ae91efad94dc401401ba27a5dcdc1871f74d881
Lorenzo Mori <lor.mori@gmail.com>
2020-11-24 23:02:14 +0000

    added whitelist

---
+++ InstaWhiteList.py
@@ -0,0 +1,37 @@
+import json
+import os.path
+
+filename = "whitelist.txt"
+
+def save(whitelist):
+
+    new_whitelist = whitelist
+
+    if(os.path.exists(filename)):
+        old_whitelist = {}
+        config = open(filename)
+        data = json.load(config)
+        old_whitelist = data["white_list"]
+
+        if(len(old_whitelist) > 0):
+            new_whitelist.append(old_whitelist)
+
+    whitelist = {}
+    whitelist["white_list"] = new_whitelist
+    config = open(filename, "w")
+    json.dump(whitelist, config)
+
+def add():
+    newWhitelist = []
+
+    while(True):
+        username = input("Enter user to whitelist: ")
+        newWhitelist.append(username)
+        print("Added user: " + username)
+
+        continueInput = input("Add new user? (y/n)")
+        if(continueInput == "n"):
+            break
+
+    save(newWhitelist)
+

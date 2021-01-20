176df68cbfda701fa3c9ffafa12f760f7b1089d4
Lorenzo Mori <lor.mori@gmail.com>
2021-01-20 20:49:12 +0000

    Pass on InstaUnfollowCheck

--- InstaUnfollowCheck.py
+++ InstaUnfollowCheck.py
@@ -2,13 +2,26 @@ from igramscraper.instagram import Instagram
 from InstaConfig import username, password
 from InstaLogin import login
 import InstaFollowers
+import time
 
 def unfollow_check(driver):
     followers = InstaFollowers.find_followers(driver)
 
+    f = open("list_followers.txt", "w")
+
     for follower in followers:
         print(follower)
-        
+        f.write("Now the file has more content!")
+    
+    f.close()
+
+    while(True):
+        time.sleep(60)
+        followers = InstaFollowers.find_followers(driver)
+
+        f = open("list_followers.txt", "r")
+        old_users = f.read().splitlines()
+
 def start():
     driver = login()
     unfollow_check(driver)

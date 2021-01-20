914963cfa6beef84c4762cf127f22e17b94c175a
Lorenzo Mori <lor.mori@gmail.com>
2021-01-20 21:56:15 +0000

    testing discord webhook

--- InstaUnfollowCheck.py
+++ InstaUnfollowCheck.py
@@ -6,6 +6,7 @@ import InstaFollowers
 import time
 from os import path
 import ast
+import discord_webhook
 
 follower_list_file = "followers_list.txt"
 
@@ -30,19 +31,38 @@ def start():
         f = open(follower_list_file, "r+")
         old_followers = f.read()
         f.close()
+        current_followers = [user.username for user in current_followers]
         old_followers = ast.literal_eval(old_followers)
 
         follower_change = len(current_followers) - len(old_followers)
 
         if follower_change != 0:
-            # send discord message here
             unfollowers = set(old_followers) - set(current_followers)
 
-            
+            message = "Your followers have changed by {}: from {} to {} \n".format(follower_change, len(old_followers), len(current_followers))
+
+            if unfollowers > 0:
+                message += '\n'.join(["https://www.instagram.com/{}".format(unfollower) for unfollower in unfollowers])
+
+            webhook = discord_webhook.DiscordWebhook(url="https://discord.com/api/webhooks/801565339976466512/cFcV4CgUY8XhOXOKGJiumuR7TDyMyi9TTGkS06riGmj87AnZZ8qO4HrW8Z8HZ6FaUYNf", content=message)
+            webhook.execute()
         
         save_followers_to_file(current_followers)
 
+def test_webhook():
+    L = instaloader.Instaloader()
+    L.login(InstaConfig.username(), InstaConfig.password())
 
+    profile = instaloader.Profile.from_username(L.context, "lollo.bot")
+    current_followers = profile.get_followers()
+
+    current_followers = [user.username for user in current_followers]
+    message = "Your followers: {} \n".format(len(current_followers))
+    message += '\n'.join(["https://www.instagram.com/{}".format(follower) for follower in current_followers])
+
+    webhook = discord_webhook.DiscordWebhook(url="https://discord.com/api/webhooks/801565339976466512/cFcV4CgUY8XhOXOKGJiumuR7TDyMyi9TTGkS06riGmj87AnZZ8qO4HrW8Z8HZ6FaUYNf", content=message)
+    webhook.execute()
 
 if __name__ == "__main__":
-    start()
+    #start()
+    test_webhook()
